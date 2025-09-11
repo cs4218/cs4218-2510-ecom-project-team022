b0c7725aacac420e52b1aba0f836b956dbcd0392
Tseng Tzu Che <tzuche2406@gmail.com>
2025-09-11 16:44:04 +0800

    Add unit tests for footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const Footer = () => {
+const Footer = ({company = "TestingComp"}) => {
   return (
     <div className="footer">
-        <h4 className="text-center">All Rights Reserved &copy; TestingComp</h4>
+        <h4 className="text-center">All Rights Reserved &copy; {company}</h4>
         <p className="text-center mt-3">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Privacy Policy</Link>
